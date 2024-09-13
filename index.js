7b2afd12d163840bfa67be864124799026052d68
devcui <devcui@outlook.com>
2024-09-13 15:55:12 +0800

    feat: add colros

--- index.js
+++ index.js
@@ -1,1 +1,163 @@
-console.log(123)
+const groups = [
+  {
+    title: "Pastel Pink to Deep Rose",
+    colors: [
+      { h: 350, s: 70, l: 90, a: 1 },
+      { h: 350, s: 70, l: 85, a: 1 },
+      { h: 350, s: 70, l: 80, a: 1 },
+      { h: 350, s: 70, l: 75, a: 1 },
+      { h: 350, s: 70, l: 70, a: 1 },
+      { h: 350, s: 70, l: 65, a: 1 },
+      { h: 350, s: 70, l: 60, a: 1 },
+      { h: 350, s: 70, l: 55, a: 1 },
+      { h: 350, s: 70, l: 50, a: 1 },
+      { h: 350, s: 70, l: 45, a: 1 },
+    ],
+  },
+  {
+    title: "Sky Blue to Navy",
+    colors: [
+      { h: 200, s: 70, l: 90, a: 1 },
+      { h: 200, s: 70, l: 85, a: 1 },
+      { h: 200, s: 70, l: 80, a: 1 },
+      { h: 200, s: 70, l: 75, a: 1 },
+      { h: 200, s: 70, l: 70, a: 1 },
+      { h: 200, s: 70, l: 65, a: 1 },
+      { h: 200, s: 70, l: 60, a: 1 },
+      { h: 200, s: 70, l: 55, a: 1 },
+      { h: 200, s: 70, l: 50, a: 1 },
+      { h: 200, s: 70, l: 45, a: 1 },
+    ],
+  },
+  {
+    title: "Mint Green to Forest Green",
+    colors: [
+      { h: 150, s: 50, l: 90, a: 1 },
+      { h: 150, s: 50, l: 85, a: 1 },
+      { h: 150, s: 50, l: 80, a: 1 },
+      { h: 150, s: 50, l: 75, a: 1 },
+      { h: 150, s: 50, l: 70, a: 1 },
+      { h: 150, s: 50, l: 65, a: 1 },
+      { h: 150, s: 50, l: 60, a: 1 },
+      { h: 150, s: 50, l: 55, a: 1 },
+      { h: 150, s: 50, l: 50, a: 1 },
+      { h: 150, s: 50, l: 45, a: 1 },
+    ],
+  },
+  {
+    title: "Sunset Orange to Brick Red",
+    colors: [
+      { h: 20, s: 80, l: 90, a: 1 },
+      { h: 20, s: 80, l: 85, a: 1 },
+      { h: 20, s: 80, l: 80, a: 1 },
+      { h: 20, s: 80, l: 75, a: 1 },
+      { h: 20, s: 80, l: 70, a: 1 },
+      { h: 20, s: 80, l: 65, a: 1 },
+      { h: 20, s: 80, l: 60, a: 1 },
+      { h: 20, s: 80, l: 55, a: 1 },
+      { h: 20, s: 80, l: 50, a: 1 },
+      { h: 20, s: 80, l: 45, a: 1 },
+    ],
+  },
+   {
+    title: "Sunset Orange to Brick Red",
+    colors: [
+      { h: 20, s: 80, l: 90, a: 1 },
+      { h: 20, s: 80, l: 85, a: 1 },
+      { h: 20, s: 80, l: 80, a: 1 },
+      { h: 20, s: 80, l: 75, a: 1 },
+      { h: 20, s: 80, l: 70, a: 1 },
+      { h: 20, s: 80, l: 65, a: 1 },
+      { h: 20, s: 80, l: 60, a: 1 },
+      { h: 20, s: 80, l: 55, a: 1 },
+      { h: 20, s: 80, l: 50, a: 1 },
+      { h: 20, s: 80, l: 45, a: 1 },
+    ],
+  },
+   {
+    title: "Sunset Orange to Brick Red",
+    colors: [
+      { h: 20, s: 80, l: 90, a: 1 },
+      { h: 20, s: 80, l: 85, a: 1 },
+      { h: 20, s: 80, l: 80, a: 1 },
+      { h: 20, s: 80, l: 75, a: 1 },
+      { h: 20, s: 80, l: 70, a: 1 },
+      { h: 20, s: 80, l: 65, a: 1 },
+      { h: 20, s: 80, l: 60, a: 1 },
+      { h: 20, s: 80, l: 55, a: 1 },
+      { h: 20, s: 80, l: 50, a: 1 },
+      { h: 20, s: 80, l: 45, a: 1 },
+    ],
+  },
+   {
+    title: "Sunset Orange to Brick Red",
+    colors: [
+      { h: 20, s: 80, l: 90, a: 1 },
+      { h: 20, s: 80, l: 85, a: 1 },
+      { h: 20, s: 80, l: 80, a: 1 },
+      { h: 20, s: 80, l: 75, a: 1 },
+      { h: 20, s: 80, l: 70, a: 1 },
+      { h: 20, s: 80, l: 65, a: 1 },
+      { h: 20, s: 80, l: 60, a: 1 },
+      { h: 20, s: 80, l: 55, a: 1 },
+      { h: 20, s: 80, l: 50, a: 1 },
+      { h: 20, s: 80, l: 45, a: 1 },
+    ],
+  },
+   {
+    title: "Sunset Orange to Brick Red",
+    colors: [
+      { h: 20, s: 80, l: 90, a: 1 },
+      { h: 20, s: 80, l: 85, a: 1 },
+      { h: 20, s: 80, l: 80, a: 1 },
+      { h: 20, s: 80, l: 75, a: 1 },
+      { h: 20, s: 80, l: 70, a: 1 },
+      { h: 20, s: 80, l: 65, a: 1 },
+      { h: 20, s: 80, l: 60, a: 1 },
+      { h: 20, s: 80, l: 55, a: 1 },
+      { h: 20, s: 80, l: 50, a: 1 },
+      { h: 20, s: 80, l: 45, a: 1 },
+    ],
+  },
+   {
+    title: "Sunset Orange to Brick Red",
+    colors: [
+      { h: 20, s: 80, l: 90, a: 1 },
+      { h: 20, s: 80, l: 85, a: 1 },
+      { h: 20, s: 80, l: 80, a: 1 },
+      { h: 20, s: 80, l: 75, a: 1 },
+      { h: 20, s: 80, l: 70, a: 1 },
+      { h: 20, s: 80, l: 65, a: 1 },
+      { h: 20, s: 80, l: 60, a: 1 },
+      { h: 20, s: 80, l: 55, a: 1 },
+      { h: 20, s: 80, l: 50, a: 1 },
+      { h: 20, s: 80, l: 45, a: 1 },
+    ],
+  },
+   {
+    title: "Sunset Orange to Brick Red",
+    colors: [
+      { h: 20, s: 80, l: 90, a: 1 },
+      { h: 20, s: 80, l: 85, a: 1 },
+      { h: 20, s: 80, l: 80, a: 1 },
+      { h: 20, s: 80, l: 75, a: 1 },
+      { h: 20, s: 80, l: 70, a: 1 },
+      { h: 20, s: 80, l: 65, a: 1 },
+      { h: 20, s: 80, l: 60, a: 1 },
+      { h: 20, s: 80, l: 55, a: 1 },
+      { h: 20, s: 80, l: 50, a: 1 },
+      { h: 20, s: 80, l: 45, a: 1 },
+    ],
+  },
+        
+];
+
+window.onload = () => {
+  const htmlContent = groups.map((group) => {
+    const blocks = group.colors.map((color) => {
+      return `<p class="color-block" style="background-color: hsla(${color.h}, ${color.s}%, ${color.l}%, ${color.a})"></p>`;
+    }).join(''); 
+    return `${blocks}<br/>`; 
+  }).join(''); 
+  document.getElementById("colors").innerHTML = htmlContent;
+};
