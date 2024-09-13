31a696a22aefb345986be5ae5f84f45881c9ae7c
devcui <devcui@outlook.com>
2024-09-13 15:58:04 +0800

    feat: add copy color

--- index.js
+++ index.js
@@ -59,105 +59,25 @@ const groups = [
       { h: 20, s: 80, l: 45, a: 1 },
     ],
   },
-   {
-    title: "Sunset Orange to Brick Red",
-    colors: [
-      { h: 20, s: 80, l: 90, a: 1 },
-      { h: 20, s: 80, l: 85, a: 1 },
-      { h: 20, s: 80, l: 80, a: 1 },
-      { h: 20, s: 80, l: 75, a: 1 },
-      { h: 20, s: 80, l: 70, a: 1 },
-      { h: 20, s: 80, l: 65, a: 1 },
-      { h: 20, s: 80, l: 60, a: 1 },
-      { h: 20, s: 80, l: 55, a: 1 },
-      { h: 20, s: 80, l: 50, a: 1 },
-      { h: 20, s: 80, l: 45, a: 1 },
-    ],
-  },
-   {
-    title: "Sunset Orange to Brick Red",
-    colors: [
-      { h: 20, s: 80, l: 90, a: 1 },
-      { h: 20, s: 80, l: 85, a: 1 },
-      { h: 20, s: 80, l: 80, a: 1 },
-      { h: 20, s: 80, l: 75, a: 1 },
-      { h: 20, s: 80, l: 70, a: 1 },
-      { h: 20, s: 80, l: 65, a: 1 },
-      { h: 20, s: 80, l: 60, a: 1 },
-      { h: 20, s: 80, l: 55, a: 1 },
-      { h: 20, s: 80, l: 50, a: 1 },
-      { h: 20, s: 80, l: 45, a: 1 },
-    ],
-  },
-   {
-    title: "Sunset Orange to Brick Red",
-    colors: [
-      { h: 20, s: 80, l: 90, a: 1 },
-      { h: 20, s: 80, l: 85, a: 1 },
-      { h: 20, s: 80, l: 80, a: 1 },
-      { h: 20, s: 80, l: 75, a: 1 },
-      { h: 20, s: 80, l: 70, a: 1 },
-      { h: 20, s: 80, l: 65, a: 1 },
-      { h: 20, s: 80, l: 60, a: 1 },
-      { h: 20, s: 80, l: 55, a: 1 },
-      { h: 20, s: 80, l: 50, a: 1 },
-      { h: 20, s: 80, l: 45, a: 1 },
-    ],
-  },
-   {
-    title: "Sunset Orange to Brick Red",
-    colors: [
-      { h: 20, s: 80, l: 90, a: 1 },
-      { h: 20, s: 80, l: 85, a: 1 },
-      { h: 20, s: 80, l: 80, a: 1 },
-      { h: 20, s: 80, l: 75, a: 1 },
-      { h: 20, s: 80, l: 70, a: 1 },
-      { h: 20, s: 80, l: 65, a: 1 },
-      { h: 20, s: 80, l: 60, a: 1 },
-      { h: 20, s: 80, l: 55, a: 1 },
-      { h: 20, s: 80, l: 50, a: 1 },
-      { h: 20, s: 80, l: 45, a: 1 },
-    ],
-  },
-   {
-    title: "Sunset Orange to Brick Red",
-    colors: [
-      { h: 20, s: 80, l: 90, a: 1 },
-      { h: 20, s: 80, l: 85, a: 1 },
-      { h: 20, s: 80, l: 80, a: 1 },
-      { h: 20, s: 80, l: 75, a: 1 },
-      { h: 20, s: 80, l: 70, a: 1 },
-      { h: 20, s: 80, l: 65, a: 1 },
-      { h: 20, s: 80, l: 60, a: 1 },
-      { h: 20, s: 80, l: 55, a: 1 },
-      { h: 20, s: 80, l: 50, a: 1 },
-      { h: 20, s: 80, l: 45, a: 1 },
-    ],
-  },
-   {
-    title: "Sunset Orange to Brick Red",
-    colors: [
-      { h: 20, s: 80, l: 90, a: 1 },
-      { h: 20, s: 80, l: 85, a: 1 },
-      { h: 20, s: 80, l: 80, a: 1 },
-      { h: 20, s: 80, l: 75, a: 1 },
-      { h: 20, s: 80, l: 70, a: 1 },
-      { h: 20, s: 80, l: 65, a: 1 },
-      { h: 20, s: 80, l: 60, a: 1 },
-      { h: 20, s: 80, l: 55, a: 1 },
-      { h: 20, s: 80, l: 50, a: 1 },
-      { h: 20, s: 80, l: 45, a: 1 },
-    ],
-  },
-        
 ];
 
 window.onload = () => {
   const htmlContent = groups.map((group) => {
     const blocks = group.colors.map((color) => {
-      return `<p class="color-block" style="background-color: hsla(${color.h}, ${color.s}%, ${color.l}%, ${color.a})"></p>`;
-    }).join(''); 
-    return `${blocks}<br/>`; 
-  }).join(''); 
+      const hslaValue = `hsla(${color.h}, ${color.s}%, ${color.l}%, ${color.a})`;
+      return `<p class="color-block" style="background-color: ${hslaValue}" onclick="copyColor('${hslaValue}')"></p>`;
+    }).join('');
+    return blocks;
+  }).join('');
   document.getElementById("colors").innerHTML = htmlContent;
 };
+
+function copyColor(color) {
+  const tempInput = document.createElement("input");
+  tempInput.value = color;
+  document.body.appendChild(tempInput);
+  tempInput.select();
+  document.execCommand("copy");
+  document.body.removeChild(tempInput);
+  alert(`Color ${color} copied to clipboard!`);
+}
